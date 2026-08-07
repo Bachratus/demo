4454e6b50e69ba259be25ca2429d89835d3657fc
bachratus <pawelbienek@gmail.com>
2026-08-07 10:40:10 +0200

    refactor: move SchedulingConfiguration to infra.config package

--- src/main/java/com/bachratus/demo/infra/config/SchedulingConfiguration.java
+++ src/main/java/com/bachratus/demo/infra/config/SchedulingConfiguration.java
@@ -1,4 +1,4 @@
-package com.bachratus.demo.infra.jobs;
+package com.bachratus.demo.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
